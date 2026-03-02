ffac9440136b37f119a5c419469f51a4dec24d96
TheLagKing <dalucanogheaven12@gmail.com>
2026-03-02 22:57:54 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,6 +1,8 @@
 #if !macro
+#if !mobile
 // Discord API
 import funkin.backend.DiscordClient;
+#end
 
 #if sys
 import sys.*;
@@ -8,6 +10,21 @@ import sys.*;
 import sys.io.*;
 #end
 
+import mobile.backend.SUtil;
+
+//Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+   
 import funkin.backend.Paths;
 
 using StringTools;
